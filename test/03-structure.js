5883fa93e795052d2fa5f03c3b8c331207b0657b
Jane Avriette <jane@cpan.org>
2015-03-13 14:13:43 -0400

    fixed a bug in the structure of the exported object

---
+++ test/03-structure.js
@@ -0,0 +1,23 @@
+var assert   = require( 'assert' )
+	, chai     = require( 'chai' )
+	, canned_argv = function () { return [
+			'node',
+			'/Users/jane/dev/jane/plugsuit/examples/eva-pilot.js',
+			'asuka',
+			'--eva-02'
+		] }
+	, plugsuit = require( '../lib/plugsuit.js' ).init( 'examples/pilots', canned_argv() );
+
+it( 'dispatch a plug', function () {
+	[ 'init', 'dispatch', 'plugs', 'logger', 'stdout', 'stderr' ].forEach( function (prop) {
+		assert( plugsuit.hasOwnProperty( prop ), 'requisite property '.concat( prop, ' present' ) );
+	} );
+	assert( typeof plugsuit.init == 'function', 'init exists and is functiony' );
+	assert( typeof plugsuit.plugs == 'object', 'plugs in plugsuit exist' );
+	plugsuit.plugs.forEach( function (plug) {
+		assert( typeof plug == 'function', 'can iterate over plugs and plugs look like functions' );
+	} );
+	assert( typeof plugsuit.stdout == 'function', 'plugsuit stdout looks functiony' );
+	assert( typeof plugsuit.stderr == 'function', 'plugsuit stderr looks functiony' );
+	assert( typeof plugsuit.logger == 'object', 'plugsuit has a logger object' );
+} );
